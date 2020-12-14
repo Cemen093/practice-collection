7b17ae0c88f41365c759ec083fd0520aac343d9c
Cemen093 <CemenP93@gmail.com>
2020-12-14 22:38:45 +0200

    fixed and done

--- src/main/java/org/itstep/MapSetTester.java
+++ src/main/java/org/itstep/MapSetTester.java
@@ -2,15 +2,19 @@ package org.itstep;
 
 import java.util.*;
 
+//оздайте проект MapLab с классом MapSetTester и статическим методом main().
 public class MapSetTester {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        //В методе main() создайте словарь HashMap с именем networkMap
+        //Ключ - имя телеканала (строка), а значение - это множество типа TreeSet,
+        // который содержит телевизионные шоу для этого телеканала - тоже строки
         Map<String, TreeSet<String>> networkMap = new HashMap<>();
 
-        //Если у вас есть словарь с каналами
+/*        //Если у вас есть словарь с каналами
         String[] arrStringResourceForNetworkMap = {"FOX", "The Simpsons", "NBC", "ER", "ABC", "20/20", "CBS", "Survivor", "ABC", "Lost", "FOX", "Family Guy", "CBS", "CSI", "FOX", "American Idol", "ABC", "Desperate Housewives", "NBC", "Law And Order"};
 
-        System.out.println("Содержимое словаря в алфавитном порядке каналов, по одному каналу в каждой строке.");
         for (int i = 0; i < arrStringResourceForNetworkMap.length; i += 2) {
             boolean IsChannelExist = false;
             for (Map.Entry<String, TreeSet<String>> entry : networkMap.entrySet()) {
@@ -26,75 +30,61 @@ public class MapSetTester {
                 newTeleShow.add(arrStringResourceForNetworkMap[i + 1]);
                 networkMap.put(arrStringResourceForNetworkMap[i], newTeleShow);
             }
-        }
-        //Выведите содержимое словаря в алфавитном порядке каналов, по одному каналу в каждой строке.
-        for (Map.Entry<String, TreeSet<String>> entry : networkMap.entrySet()) {
-            System.out.println(entry);
-
-        }
-        System.out.println();
-        //Создайте массив содержащий ключи (названия каналов) словаря `networkMap`.
-        String[] arrKeyChannel = new String[networkMap.size()];
-        networkMap.keySet().toArray(arrKeyChannel);
-
-        System.out.println("Массив до сортировки");
-        for (int i = 0; i < arrKeyChannel.length; i++) {
-            System.out.print(arrKeyChannel[i] + ", ");
-        }
-        System.out.println("\n");
-        //Отсортируйте полученный массив каналов по алфавиту.
-        //Ну как бы он уже по алфавиту, но ладно
-        Arrays.sort(arrKeyChannel);
-
-        System.out.println("Массив после сортировки");
-        for (int i = 0; i < arrKeyChannel.length; i++) {
-            System.out.print(arrKeyChannel[i] + ", ");
-        }
-        System.out.println("\n");
-
-        //Переберите элементы массива ключей, что бы получить соответствующее значение (наборы ТВ-шоу) для каждого ключа из словаря и распечатать их.
-        System.out.println("Наборы ТВ-шоу");
-        for (int i = 0; i < arrKeyChannel.length; i++) {
-            System.out.println(networkMap.get(arrKeyChannel[i]));
-        }
-        System.out.println("\n");
-
-        //**Примечание**: Вам не придется сортировать ТВ-шоу для канала, так как они уже будут отсортированы. Как вы думаете почему?
-        //Так уже и не припомню это из лекции, но по моему из-за особеностей хеширования ключа
-
-        networkMap.clear();
-
-        System.out.println("Please enter at least 10 TV shows");
-        for (int i = 0; i < 10; i++) {
-            Scanner scanner = new Scanner(System.in);
-
-            System.out.print("Input tv network: ");
-            String nameChannel = scanner.nextLine();
-
-            System.out.print("Input tv show on " + nameChannel + ": ");
-            String nameTvShow = scanner.nextLine();
+        }*/
 
 
-            boolean IsChannelExist = false;
-            for (Map.Entry<String, TreeSet<String>> entry : networkMap.entrySet()) {
-                if (entry.getKey().equals(nameChannel)) {
-                    IsChannelExist = true;
-                    entry.getValue().add(nameTvShow);
-                    break;
-                }
+        //boolean containsKey(Object k): возвращает true, если коллекция содержит ключ k
+        //boolean containsValue(Object v): возвращает true, если коллекция содержит значение v
+        //Set<Map.Entry<K, V>> entrySet(): возвращает набор элементов коллекции. Все элементы представляют объект Map.Entry
+        //boolean isEmpty: возвращает true, если коллекция пуста
+        //V putIfAbsent(K k, V v): помещает в коллекцию новый объект с ключом k и значением v, если в коллекции еще нет элемента с подобным ключом.
+        //void putAll(Map<? extends K, ? extends V> map): добавляет в коллекцию все объекты из отображения map
+        int numberShowAdd;
+        do {
+            System.out.println("Please enter how many shows you want to add");
+            numberShowAdd = Integer.parseInt(scanner.next());
+
+            //Таким образом введите не менее 10 телевизионных шоу.
+            if (numberShowAdd < 10){
+                System.out.println("The number must be greater than 10");
+            }
+        } while (numberShowAdd < 10);
+        for (int i = 0; i < numberShowAdd; i++) {
+
+            //В цикле запросите у пользователя название телешоу,
+            // а затем запросите пользователя ввести имя телевизионного канала для этого шоу
+            System.out.print("Input TV network and TV show ");
+            String nameChannel = scanner.next();
+            String nameTvShow = scanner.next();
+
+            TreeSet<String> newTeleShow = new TreeSet<>();
+            newTeleShow.add(nameTvShow);
+
+            //Если канал еще не добавлен в словарь, то создайте множество TreeSet в которое добавьте название телешоу,
+            // а затем ключ к паре значений (канал, с заданным телевизионным шоу) в словарь
+            if (networkMap.putIfAbsent(nameChannel, newTeleShow) != null) {
+                //Если канал находится в словаре, добавьте телешоу к множеству TreeSet этого телеканала.
+                networkMap.get(nameChannel).add(nameTvShow);
             }
 
-            if (!IsChannelExist) {
-                TreeSet<String> newTeleShow = new TreeSet<>();
-                newTeleShow.add(nameTvShow);
-                networkMap.put(nameChannel, newTeleShow);
-            }
-
+            //После обновления словаря выведите его содержимое.
             System.out.println(networkMap.toString());
-            //(обратите внимание, что сети не расположены в алфавитном порядке, но телевизионные
-            //   шоу для каждого канала выводятся по алфавиту. Как вы думаете почему?)
-            //У меня и то и то в алфавитном порядке идёт (я так понимаю из-за HashMap) и я не знаю каким методом вывода вы пользовались так что без понятия
-            //может добавление компоратора при создании? new TreeSet<>(Comparator.comparing(String::length));
+        }
+
+        //Если у вас есть словарь с каналами
+        if (!networkMap.isEmpty()){
+            //выведите содержимое словаря в алфавитном порядке каналов, по одному каналу в каждой строке.
+
+            //Создайте массив содержащий ключи (названия каналов) словаря networkMap.
+            String[] keys = networkMap.keySet().toArray(new String[0]);
+            //Отсортируйте полученный массив каналов по алфавиту.
+            Arrays.sort(keys);
+
+            //Переберите элементы массива ключей
+            for (int i = 0; i < networkMap.size(); i++) {
+                //получить соответствующее значение (наборы ТВ-шоу) для каждого ключа из словаря и распечатать их
+                System.out.println("TV " + keys[i] + " " + networkMap.get(keys[i]));
+            }
         }
     }
 }
